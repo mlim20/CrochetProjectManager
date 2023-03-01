48ac2ff5221cf20e9bf11ca9e0e6cff3ad91e995
mlim20 <mlim20@student.ubc.ca>
2023-03-01 14:45:23 -0800

    Removed rows variables, changed yarns and otherMaterials to ArrayLists, make constructors for Project and Yarn

--- src/model/Project.java
+++ src/model/Project.java
@@ -2,20 +2,38 @@ package model;
 
 import java.awt.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Project {
     private String name;
     private LocalDateTime startDateTime;
     private LocalDateTime endDateTime;
-    private int totalRows;
-    private int currentRows;
-    private Yarn[] yarns;
-    private String[] otherMaterials;
+
+    // what if project has more than one part?
+//    private int totalRows;
+//    private int currentRows;
+    private List<Yarn> yarns;
+    private List<String> otherMaterials;
     private double hookSize;
+
+    // pattern or link to pattern?
+
+    public Project(String name) {
+        this.name = name;
+        yarns = new ArrayList<>();
+        otherMaterials = new ArrayList<>();
+    }
 
     private class Yarn {
         private String name;
         private Color color;
         private String brand;
+
+        public Yarn(String name, Color color, String brand) {
+            name = this.name;
+            color = this.color;
+            brand = this.brand;
+        }
     }
 }
