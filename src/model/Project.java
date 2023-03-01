e0fdc2c716991366a24cc8da63e935e52ee94925
mlim20 <mlim20@student.ubc.ca>
2023-02-28 18:08:08 -0800

    Added variables name, start and end date/time, total and current rows, yarn, hook, materials in Project class Added Yarn class to Project class (variables: name, color, brand)

--- src/model/Project.java
+++ src/model/Project.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.awt.*;
+import java.time.LocalDateTime;
+
 public class Project {
     private String name;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private int totalRows;
+    private int currentRows;
+    private Yarn[] yarns;
+    private String[] otherMaterials;
+    private double hookSize;
+
+    private class Yarn {
+        private String name;
+        private Color color;
+        private String brand;
+    }
 }
